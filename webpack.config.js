d6e79d025b988e3ae51c5e8e2673338646bbe19d
enrightcw <58670012+enrightcw@users.noreply.github.com>
2023-03-23 12:24:44 -0400

    More webpack updates

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,47 @@
+const miniCssExtractPlugin = require('mini-css-extract-plugin')
 const path = require('path')
 
 module.exports = {
-  entry: './src/js/main.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  devServer: {
-    static: path.resolve(__dirname, 'dist'),
-    port: 8080,
-    hot: true
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(scss)$/,
-        use: [
-          {
-            loader: 'style-loader'
-          },
-          {
-            loader: 'css-loader'
-          },
-          {
-            loader: 'postcss-loader',
-            options: {
-              postcssOptions: {
-                plugins: () => [
-                  require('autoprefixer')
-                ]
-              }
+    mode: 'development',
+    entry: './src/js/main.js',
+    plugins: [new miniCssExtractPlugin()],
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devServer: {
+        static: path.resolve(__dirname, 'dist'),
+        port: 8080,
+        hot: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(scss)$/,
+                use: [
+                {
+                    // Extracts CSS for each JS file that includes CSS
+                    loader: miniCssExtractPlugin.loader
+                },
+                {
+                    loader: 'css-loader'
+                },
+                {
+                    loader: 'postcss-loader',
+                    options: {
+                    postcssOptions: {
+                        plugins: () => [
+                        require('autoprefixer')
+                        ]
+                    }
+                }
+            },
+            {
+                loader: 'sass-loader'
             }
-          },
-          {
-            loader: 'sass-loader'
-          }
         ]
       }
     ]
   }
 }
+
